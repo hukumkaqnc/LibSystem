f22f3b58b20803a83662cd8aab82898db745caae
hukumkaqnc <94224516+hukumkaqnc@users.noreply.github.com>
2023-10-11 12:07:22 +0300

    Add files via upload

---
+++ WrReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LibSystem
+{
+    internal class WrReader
+    {
+        public string[] LocalArr = { };
+        string path = "note1.txt";
+        public WrReader(string val) {
+            path = val;
+                    
+        }
+        public void FileConverter()
+        {
+            string[] result = { };
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    result.Append(line);
+                }
+            }
+            LocalArr = result;
+        }
+        public void FileWriter() {
+
+            using (StreamWriter writer = new StreamWriter(path, false))
+            {
+                foreach (string line in LocalArr)
+                {
+                    writer.WriteLineAsync(line);
+                }
+                
+            }
+        }
+    }
+}
